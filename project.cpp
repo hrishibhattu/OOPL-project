d02961eb42c32dca38447b20be1fe5a29abb21be
Anurag Pandey <anuragcarbon1998@gmail.com>
2018-10-09 23:06:24 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
+
 using namespace std;
+
+class superMarket {
+  void addItem();
+  void deleteItem();
+  void editItem();
+  void displayall();
+  void findItem();
+  void billCalculation();
+};
+  
